d849ac65f014412a0f8e81981c947ce815b30c60
Ody950 <odai.al.obeidat950@gmail.com>
2023-05-25 16:02:55 +0000

    odaiday2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import Home from './components/Home';
 import Restaurants from './components/Restaurants';
 import Header from './components/Header';
 import FRestaurants from './components/FRestaurants';
+import TheRestaurant from './components/TheRestaurant';
 
 
 function App() {
   return (
     <>
- 
+
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/Restaurants' element={<Restaurants/>}></Route>
       <Route path="/FRestaurants" element={<FRestaurants/>}></Route>
-   
+      <Route path="/TheRestaurant/:id" element={<TheRestaurant/>}></Route>
     </Routes>
     </>
   )
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -5,15 +5,35 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import TheRestaurant from './TheRestaurant';
+
+
 
 
 function Restaurants() {
   const [ammanData, setAmmanData] = useState([]);
   const [irbidData, setIrbidData] = useState([]);
   const [aqabaData, setAqabaData] = useState([]);
+  const [wadiRummData, setwadiRummData] = useState([]);
+  const [petraData, setpetraData] = useState([]);
+
+
+
+
+
+  const [clickedrestaurant, setclickedrestaurant] = useState({});
+
+  const restaurantShow = (item) => {
+     
+    setclickedrestaurant(item);
+  }
+
+
+
+
 
   useEffect(() => {
-    const serverURL = `http://localhost:3002/?lat=31.945335&long=35.886671`;
+    const serverURL = `http://localhost:3026/?lat=31.945335&long=35.886671`;
     fetch(serverURL)
       .then((response) => {
         response.json().then((data) => {
@@ -22,7 +42,7 @@ function Restaurants() {
         });
       });
 
-    const serverURL2 = `http://localhost:3002/?lat=32.551445&long=35.851479`;
+    const serverURL2 = `http://localhost:3026/?lat=32.551445&long=35.851479`;
     fetch(serverURL2)
       .then((response) => {
         response.json().then((data) => {
@@ -31,7 +51,7 @@ function Restaurants() {
         });
       });
 
-    const serverURL3 = `http://localhost:3002/?lat=29.52667&long=35.00778`;
+    const serverURL3 = `http://localhost:3026/?lat=29.52667&long=35.00778`;
     fetch(serverURL3)
       .then((response) => {
         response.json().then((data) => {
@@ -39,31 +59,62 @@ function Restaurants() {
           setAqabaData(data);
         });
       });
-  }, []);
 
-  const states = ['Amman', 'Irbid', 'Aqaba'];
+
+
+    const serverURL4 = `http://localhost:3026/?lat=29.542474&long=35.394125`;
+    fetch(serverURL4)
+      .then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+          setwadiRummData(data);
+        });
+      });
+
+
+
+          const serverURL5 = `http://localhost:3026/?lat=30.324270&long=35.462641`;
+          fetch(serverURL5)
+            .then((response) => {
+              response.json().then((data) => {
+                console.log(data);
+                setpetraData(data);
+
+              });
+            });
+
+
+
+        }, []);
+
+  const states = ['Amman', 'Irbid', 'Aqaba', 'wadiRumm', 'petra'];
   const [selectedState, setSelectedState] = useState(states[0]);
   const restaurants = [
     { data: ammanData, state: 'Amman' },
     { data: irbidData, state: 'Irbid' },
-    { data: aqabaData, state: 'Aqaba' }
+    { data: aqabaData, state: 'Aqaba' },
+    { data: wadiRummData, state: 'wadiRumm' },
+    { data: petraData, state: 'petra' },
   ];
 
   const filteredData = restaurants.filter((restaurant) => restaurant.state === selectedState);
 
-  // const path = 'https://image.tmdb.org/t/p/w500';
+
 
   return (
     <>
+   
       <h1>Restaurants</h1>
       <Link to="/">
         <Button type="submit">Home</Button>
       </Link>
-       <h1>{selectedState}</h1>
+      <h1>{selectedState}</h1>
       <div>
         <button onClick={() => setSelectedState('Amman')}>{'Amman'}</button>
         <button onClick={() => setSelectedState('Irbid')}>{'Irbid'}</button>
         <button onClick={() => setSelectedState('Aqaba')}>{'Aqaba'}</button>
+        <button onClick={() => setSelectedState('wadiRumm')}>{'wadiRumm'}</button>
+        <button onClick={() => setSelectedState('petra')}>{'petra'}</button>
       </div>
 
       <div>
@@ -71,27 +122,31 @@ function Restaurants() {
 
           return (
             <>
+            {/* <Link><TheRestaurant  clickedrestaurant={clickedrestaurant}/></Link> */}
+
               {restaurant.data.map((item) => {
-                if(item !== null){
-
+                if (item !== null) {
+                  return (
+                    <>
+                      <Link  to={`/TheRestaurant/${item.location_id}`}>
+                        <section key={item.name + 1} className='Movie1'>
+                          < Card className='card1' key={item.name + 1} >
+                            <Card.Img width="200px" className='image1' variant="top" src={item.photo} />
+                            <Card.Body>
+                              <Card.Title className='text1'>{item.name}</Card.Title>
+                             
+                            </Card.Body>
+                          </Card >
+                        </section>
+                        
+                      </Link>
+                    </>
+                  )
+                }
                 
-                return (
-                  <section key={item.name+1} className='Movie1'>
-                    < Card className='card1' key={item.name+1} >
-                      <Card.Img width="200px" className='image1' variant="top" src={ item.photo} />
-                      <Card.Body>
-                        <Card.Title className='text1'>{item.name}</Card.Title>
-
-                      </Card.Body>
-                    </Card >
-                  </section>
-
-
-                )
-              }
               })}
 
-
+             
 
 
             </>
@@ -99,7 +154,8 @@ function Restaurants() {
         }
         )}
       </div>
-
+      
+    
     </>
   )
 }

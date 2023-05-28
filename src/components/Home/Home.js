2cda710525a3a7b09596b14f586e1c4f87115eb7
Ody950 <odai.al.obeidat950@gmail.com>
2023-05-28 18:18:10 +0000

    day6

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,11 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
-
+import "./Home.css";
+import imageamman from '../Img/amman.webp'
+import imageaqaba from '../Img/aqaba.jpeg'
+import imageirbid from '../Img/irbid.jpeg'
+import imagepetra from '../Img/petra.jpg'
 function Home() {
 
 
@@ -18,30 +21,30 @@ function Home() {
 
   //12909961, '1181235', '11772898', '4359069', '12434409', '2287470', '2406112', '1135125'
   useEffect(() => {
-    const idArr = ['12909961','1181235'];
-const idArrall = [];
-for (let i = 0; i < idArr.length; i++) {
-  setTimeout(() => {
-    const serverURL = `${process.env.REACT_APP_serverURL}/getResturauntById?location=${idArr[i]}`;
-    fetch(serverURL)
-      .then((response) => {
-        response.json().then((data) => {
-          console.log(data);
-          idArrall.push(data);
-          if (idArrall.length === idArr.length) {
-            setRestaurantData(idArrall);
-          }
-        });
-      });
-  }, 250 * i);
-}
+    const idArr = ['12909961', '1181235'];
+    const idArrall = [];
+    for (let i = 0; i < idArr.length; i++) {
+      setTimeout(() => {
+        const serverURL = `${process.env.REACT_APP_serverURL}/getResturauntById?location=${idArr[i]}`;
+        fetch(serverURL)
+          .then((response) => {
+            response.json().then((data) => {
+              console.log(data);
+              idArrall.push(data);
+              if (idArrall.length === idArr.length) {
+                setRestaurantData(idArrall);
+              }
+            });
+          });
+      }, 250 * i);
+    }
 
   }, []);
 
 
 
 
-// '15079947', '5863643', '2221012', '2386844', '7594311', '18956821', '2429468'
+  // '15079947', '5863643', '2221012', '2386844', '7594311', '18956821', '2429468'
   useEffect(() => {
     const idArr = ['1371269'];
     const idArrall = [];
@@ -75,7 +78,7 @@ for (let i = 0; i < idArr.length; i++) {
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
-  
+
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -83,7 +86,7 @@ for (let i = 0; i < idArr.length; i++) {
       alert('Geolocation is not supported by this browser.');
     }
   };
- 
+
   const showPosition = async (position) => {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
@@ -96,165 +99,162 @@ for (let i = 0; i < idArr.length; i++) {
         });
       });
   };
-  
- 
 
-  
+
+
+
 
   //Sidebar-end------------------------------------------------------------------------------------
 
   return (
-   
+
     <>
+      <section id="sectionmainMain">
 
-     
-      <h1>Home</h1>
-      <section class="section0">
-        <div className="sidebar">
-          <form>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} />
 
-            <button type="button" onClick={getLocation}>Get Location</button>
+        <section id="sectionmain" style={{ width: '80%' }}>
 
-          </form>
-        </div>
-        
-         {aroundYouData.map((item) => {
-          if (item !== null) {
-               
-                  return (
-                    <>
-                      <Link  to={`/TheRestaurant/${item.location_id}`}>
-                        <section key={item.name + 1} className='Movie1'>
-                          < Card className='card1' key={item.name + 1} >
-                            <Card.Img width="200px" className='image1' variant="top" src={item.photo} />
-                            <Card.Body>
-                              <Card.Title className='text1'>{item.name}</Card.Title>
-                             
-                            </Card.Body>
-                          </Card >
-                        </section>
-                        
-                      </Link>
-                    </>
-                  )
-        
-              }
-              })} 
           
-      </section>
+
+          <section id="section1">
+               <div id="cardmain">
+
+           <div id="img1"  class="homeCard1" >
+                <a  href="./Restaurants">
+                 <p>xxxxxxxxxxxxxxxx</p>
+                </a>
+            </div>
+            
+           <div id="img2"   class="homeCard1"  >
+                <a href="./Restaurants">
+                 <p>xxxxxxxxxxxxxxxx</p>
+                </a>
+            </div>
+
+            <div id="divcardmain">
+            <div  id="img3" class="homeCard1" >
+                <a href="./Restaurants">
+                 <p>xxxxxxxxxxxxxxxx</p>
+                </a>
+            </div>
+            <div  id="img4" class="homeCard1" >
+                <a href="./Restaurants" alt="img4">
+                 <p>xxxxxxxxxxxxxxxx</p>
+                </a>
+            </div>
+
+            </div>
+
+            </div>
+         
+          </section>
+
+          <section id="section2">
+            <h1>Five star restaurants around the kingdom</h1>
+            {RestaurantData.map(item => {
+
+              return (
+                <>
+                  <Link to={`/TheRestaurant/${item.location_id}`}>
+                    <section key={item.name + 1} >
+                      < div  class="divcard" key={item.name + 1} >
+                        <img class="imgcard"  id="Imagsection2" variant="top" src={item.photo} />
+                        <h1 class="namecard">{item.name}</h1>
+                      </div >
+                    </section>
+
+                  </Link>
+                </>
+              )
+
+            })}
+          </section>
+
+          <Figure>
+            <Figure.Image
+              width={171}
+              height={180}
+              alt="171x180"
+              src="holder.js/171x180"
+            />
+            <Figure.Caption>
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </Figure.Caption>
+          </Figure>
+
+          <section id="section3">
+            <Row xs={1} md={3} >
+              {RestaurantData2.map(item => {
+                return (
+                  <Link to={`/TheRestaurant/${item.location_id}`}>
+                    <section key={item.name + 1} >
+                      < Card  key={item.name + 1} >
+                        <Card.Img  id="Imagsection3" variant="top" src={item.photo} />
+                        <Card.Body>
+                          <Card.Title >{item.name}</Card.Title>
+
+                        </Card.Body>
+                      </Card >
+                    </section>
+
+                  </Link>
+                )
+              })}
+            </Row>
+          </section>
+          <Figure>
+            <Figure.Image
+              width={171}
+              height={180}
+              alt="171x180"
+              src="holder.js/171x180"
+            />
+            <Figure.Caption>
+              Nulla vitae elit libero, a pharetra augue mollis interdum.
+            </Figure.Caption>
+          </Figure>
+
+          <section class="section4">
+            <Button href="#" type="submit">About</Button>{' '}
+            <Button href="#" type="submit">News</Button>{' '}
+            <Button href="#" type="submit">Contact Us</Button>
+          </section>
+        </section>
 
 
-      <section class="section1">
-        <Card class="Card1" style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Link to="./Restaurants">
-              <Button type="submit">Button</Button>
-            </Link>
-          </Card.Body>
-        </Card>
-        <Card class="Card2" style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Link to="./Restaurants">
-              <Button type="submit">Button</Button>
-            </Link>
-          </Card.Body>
-        </Card>
-        <div>
-          <Card class="Card3" style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Link to="./Restaurants">
-                <Button type="submit">Button</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-          <Card class="Card4" style={{ width: '18rem' }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Link to="./Restaurants">
-                <Button type="submit">Button</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </div>
-      </section>
+        <section id="section0" style={{ width: '20%' }}>
+          <div >
+            <form>
+              <label htmlFor="button">Click on the locate button to find restaurants near you:</label>
+              <button type="button" onClick={getLocation}>Get Location</button>
 
-      <section class="section2">
-        <h1>Five star restaurants around the kingdom</h1>
-        {RestaurantData.map(item => {
-     
-          return (
-            <>
-            <Link  to={`/TheRestaurant/${item.location_id}`}>
-              <section key={item.name + 1} className='Movie1'>
-                < Card className='card1' key={item.name + 1} >
-                  <Card.Img width="200px" className='image1' variant="top" src={item.photo} />
-                  <Card.Body>
-                    <Card.Title className='text1'>{item.name}</Card.Title>
-                   
-                  </Card.Body>
-                </Card >
-              </section>
-              
-            </Link>
-          </>
-          )
-      
-        })}
-      </section>
+            </form>
+          </div>
 
-      <Figure>
-        <Figure.Image
-          width={171}
-          height={180}
-          alt="171x180"
-          src="holder.js/171x180"
-        />
-        <Figure.Caption>
-          Nulla vitae elit libero, a pharetra augue mollis interdum.
-        </Figure.Caption>
-      </Figure>
+          {aroundYouData.map((item) => {
+            if (item !== null) {
 
-      <section class="section3">
-        <Row xs={1} md={3} className="g-4">
-          {RestaurantData2.map(item => {
-            return (
-              <Link  to={`/TheRestaurant/${item.location_id}`}>
-              <section key={item.name + 1} className='Movie1'>
-                < Card className='card1' key={item.name + 1} >
-                  <Card.Img width="200px" className='image1' variant="top" src={item.photo} />
-                  <Card.Body>
-                    <Card.Title className='text1'>{item.name}</Card.Title>
-                   
-                  </Card.Body>
-                </Card >
-              </section>
-              
-            </Link>
-            )
+              return (
+                <>
+                  <Link to={`/TheRestaurant/${item.location_id}`}>
+                    <section key={item.name + 1} >
+                      < Card  key={item.name + 1} >
+                        <Card.Img width="200px" variant="top" src={item.photo} />
+                        <Card.Body>
+                          <Card.Title >{item.name}</Card.Title>
+
+                        </Card.Body>
+                      </Card >
+                    </section>
+
+                  </Link>
+                </>
+              )
+
+            }
           })}
-        </Row>
-      </section>
-      <Figure>
-        <Figure.Image
-          width={171}
-          height={180}
-          alt="171x180"
-          src="holder.js/171x180"
-        />
-        <Figure.Caption>
-          Nulla vitae elit libero, a pharetra augue mollis interdum.
-        </Figure.Caption>
-      </Figure>
 
-      <section class="section4">
-        <Button href="#" type="submit">About</Button>{' '}
-        <Button href="#" type="submit">News</Button>{' '}
-        <Button href="#" type="submit">Contact Us</Button>
+        </section>
       </section>
     </>
   )

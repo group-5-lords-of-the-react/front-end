cb8ff594f12de9f37332e4d0b3f995387a32c827
Abdelrahman <godgold11@gmail.com>
2023-05-28 18:12:54 +0000

    day5 frontend styling

--- src/components/Booking/Booking.js
+++ src/components/Booking/Booking.js
@@ -1,110 +1,109 @@
-
 import { useState, useEffect } from 'react';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import ModalUpdat from "../ModalUpdat/ModalUpdat";
+import "./Booking.css";
 
 function Booking() {
-
-  const [BookingArr, setBookingArr] = useState([]);
-  const [updateBooking, setupdateBooking] = useState(false);
-  const [passData, setpassData] = useState([]);
-
-  const [newArr, setNewArr] = useState([])
+  const [bookingArr, setBookingArr] = useState([]);
+  const [updateBooking, setUpdateBooking] = useState(false);
+  const [passData, setPassData] = useState([]);
+  const [newArr, setNewArr] = useState([]);
 
   const funUpdateBook = (item) => {
-    setupdateBooking(true)
-    setpassData(item)
-
-  }
-
+    setUpdateBooking(true);
+    setPassData(item);
+  };
 
   const handleClose = () => {
-    setupdateBooking(false)
-  }
-
+    setUpdateBooking(false);
+  };
 
   const getBooking = () => {
     const serverURL = `${process.env.REACT_APP_serverURL}/bookingList`;
     fetch(serverURL)
       .then((response) => {
-        response.json()
-          .then(data => {
-            setBookingArr(data)
-            console.log(data)
-          })
-      })
-  }
-
-
-  const fundeleteBook = (item) => {
-  
-    const serverURL = `${process.env.REACT_APP_serverURL}/deleteBooking/${item}`;
-    axios.delete(serverURL)
-      .then(response => {
-        getBooking()
-        console.log(response.data)
-
+        response.json().then((data) => {
+          setBookingArr(data);
+          console.log(data);
+        });
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
+      });
+  };
+
+  const funDeleteBook = (item) => {
+    const serverURL = `${process.env.REACT_APP_serverURL}/deleteBooking/${item}`;
+    axios
+      .delete(serverURL)
+      .then((response) => {
+        getBooking();
+        console.log(response.data);
       })
-
-  }
-
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const takeNewDataFromUpdatedModal = (arr) => {
-    setNewArr(arr)
-  }
-  useEffect(() => {
-    setNewArr(BookingArr)
-  }, [BookingArr])
+    setNewArr(arr);
+  };
 
   useEffect(() => {
-    getBooking()
-  }, [])
+    setNewArr(bookingArr);
+  }, [bookingArr]);
 
+  useEffect(() => {
+    getBooking();
+  }, []);
 
   return (
-
     <>
-        {/* change card display to table */}
-      <h1>Booking List</h1>
-      {newArr.map(item => {
-        return (
-          <div className='container'>
-      <div className='row'>
-        {newArr.map((item) => {
-          return (
-            <div key={item.r_location_id} className='col-md-4'>
-              <section className='Restaurant'>
-                <Card className='Restaurant-Booking-ID' key={item.r_location_id}>
-                  <Card.Body>
-                    <Card.Title className='REST-Name'>{item.r_name}</Card.Title>
-                    <Card.Text>
-                      <p className='REST-Address'>{item.r_address}</p>
-                      <p>{item.r_reservation_date}</p>
-                      <p>{item.r_reservation_time}</p>
-                      <p>{item.no_people_reservation}</p>
-                    </Card.Text>
-                    <Button variant='primary' onClick={() => { funUpdateBook(item) }}>Update</Button>
-                    <Button variant='primary' onClick={() => { fundeleteBook(item.r_location_id) }}>Delete</Button>
-                  </Card.Body>
-                </Card>
-              </section>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-    
-        )
-      })}
+      <h2>Booking List</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Restaurant</th>
+            <th>Address</th>
+            <th>Reservation Date</th>
+            <th>Reservation Time</th>
+            <th>No. of People</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {newArr.map((item) => (
+            <tr key={item.r_location_id}>
+              <td>{item.r_name}</td>
+              <td>{item.r_address}</td>
+              <td>{item.r_reservation_date}</td>
+              <td>{item.r_reservation_time}</td>
+              <td>{item.no_people_reservation}</td>
+              <td className='button-alignment'>
+                <Button variant="primary" onClick={() => funUpdateBook(item)}>
+                  Update
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={() => funDeleteBook(item.r_location_id)}
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-      <ModalUpdat updateBooking={updateBooking} passData={passData} handleClose={handleClose} takeNewDataFromUpdatedModal={takeNewDataFromUpdatedModal} />
+      <ModalUpdat
+        updateBooking={updateBooking}
+        passData={passData}
+        handleClose={handleClose}
+        takeNewDataFromUpdatedModal={takeNewDataFromUpdatedModal}
+      />
     </>
-  )
+  );
 }
 
 export default Booking;
